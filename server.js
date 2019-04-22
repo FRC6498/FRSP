3f9eb20ecfb1c2e5ae03ea9682ffb5c76d72ce05
DeltaDizzy <EJBraswell@gmail.com>
2019-04-22 11:36:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http);
 
 const port = 80
-const ip = '0.0.0.0'
+const ip = "0.0.0.0"
 const server = http.createServer(function(req, res) { })
   .listen(port, ip, () => {
   console.log('App created on port ${port} at IP Address ${ip}');
