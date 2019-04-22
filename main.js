603dc2740101a6231b56f9afcfe4e8eca4a8dcd4
DeltaDizzy <EJBraswell@gmail.com>
2019-04-22 11:46:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
-// issac and/or isaac feel free to do everything here thanks
-var startMsg = "FRSP STARTED";
-var newVal;
-var ele;
-
-function Log(msg)
-{
-    if(msg == typeof(String))
-    {
-        Console.Log("[FRSP]: " + msg);
-    }
-}
-function Start()
-{
-    Log(startMsg);
-    alert("FIRST RSP STARTED");
-}
-
-window.onload = Start;
-
-function incrementValue(id) {
-    if(id == typeof(String)) {
-        var val = parseInt(document.getElementById(id).value, 10);
-        val = isNaN(val) ? 0 : val;
-        val++;
-        document.getElementById(id).value = val;
-    } else {
-        throw new TypeError("[FRSP] The id provided was not a string");
-    }
+function increment(displayId) {
+    var display = $(displayId);
+    display.value += 1;
 }
